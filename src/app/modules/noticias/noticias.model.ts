e73daf4be62594c33edc8fe02f3a0e2d86cf9122
MarkelCA <cuestaarribas.markel@gmail.com>
2022-04-27 17:34:52 +0200

    servicio noticias

---
+++ src/app/modules/noticias/noticias.model.ts
@@ -0,0 +1,6 @@
+export interface Noticia {
+    nid         : [object]
+    title       : [object]
+    created     : [object]
+    body        : [object]
+}
--- src/app/modules/noticias/noticias.service.ts
+++ src/app/modules/noticias/noticias.service.ts
@@ -1,9 +1,26 @@
+import {HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {Subject} from 'rxjs';
+import {Noticia} from './noticias.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoticiasService {
 
-  constructor() { }
+    private noticias : Noticia[] = []
+    constructor(private http : HttpClient) {}
+
+    private noticiasUpdated = new Subject<Noticia[]>()
+
+    getNoticias() {
+        const testPostRequest = this.http.get<Noticia[]>("http://elhuyar.desarrollo.com/eu/api/noticias?_format=json")
+        testPostRequest.subscribe(data => {
+            this.noticias = data
+            this.noticiasUpdated.next(data)
+        })
+        // We use the spread operator to make a copy of posts instead of passing the reference
+        // so it won't be affected on modifications
+        return [...this.noticias]
+    }
 }
